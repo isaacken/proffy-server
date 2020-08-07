b64545f7282222b723b2661943385a9385eec31c
Isaac Kennedy <isaackennedyfds@gmail.com>
2020-08-07 08:01:21 -0300

    Remove setup database prod script

--- reset-database.sh
+++ reset-database.sh
@@ -1,4 +1,4 @@
 rm ./src/database/database.sqlite
 touch ./src/database/database.sqlite
-yarn knex migrate:latest
+yarn knex migrate:latest  
 sudo chmod -R 777 ./src/database/database.sqlite
--- setup-database-prod.sh
+++
@@ -1,3 +0,0 @@
-touch ./src/database/database.sqlite
-yarn knex migrate:latest --knexfile ./dist/knexfile.js
-chmod -R 777 ./src/database/database.sqlite
