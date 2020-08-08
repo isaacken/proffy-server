a26df9911b8d2f11f0dde6c6c94e0f7982d1e617
Isaac Kennedy <isaackennedyfds@gmail.com>
2020-08-07 21:57:33 -0300

    Test route

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
-import express from 'express';
+import express, { response } from 'express';
 import ClassesController from './controllers/ClassesController';
 import ConnectionsController from './controllers/ConnectionsController';
+import db from './database/connection';
 
 const routes = express.Router();
 
@@ -12,5 +13,14 @@ routes.post('/classes', classesController.create);
 
 routes.get('/connections', connectionsController.index);
 routes.post('/connections', connectionsController.create);
+
+routes.get('/teste', function () {
+  const query =
+    "SELECT name FROM sqlite_master WHERE type='table' ORDER BY name";
+
+  const r = db.raw(query);
+
+  return response.json(r);
+});
 
 export default routes;
